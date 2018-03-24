2ea9a48c369711b4eacca21c928bc309a954756e
Mark Alaniz <markalaniz000@gmail.com>
2018-03-23 23:50:13 -0500

    added margins to card tops and closed margins on mobile

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,7 +2,7 @@ import React from "react";
 import './CardList.css';
 
 const CardList = props => (
-   <section className="columns is-multiline is-centered card-list">{props.children}</section>
+   <section className="columns is-multiline is-centered is-mobile">{props.children}</section>
 );
 
 export default CardList;
