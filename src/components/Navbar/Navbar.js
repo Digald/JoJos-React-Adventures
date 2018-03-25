fb226dc4e7b6d3e3b13644af4ea1b077d523340f
Mark Alaniz <markalaniz000@gmail.com>
2018-03-24 20:52:46 -0500

    removed unused images and removed image folder from public. added image folder locally to Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Navbar.css";
+import menacing from './images/menacing.png';
 
 const Navbar = props => (
   <nav className="level is-fixed-top navbar is-mobile">
     <div className="level-item has-text-centered">
-      <img className="brand-img" src="/images/menacing.png" alt="menacing" />
+      <img className="brand-img" src={menacing} alt="menacing" />
       <p className="brand-name">JJRA</p>
     </div>
     <p className="level-item has-text-centered">Score: {props.score}</p>
